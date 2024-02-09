b160e30ad3bc749750fdaa388617956338d6c61a
parapsychic <febinkdominic@outlook.com>
2024-02-09 07:45:29 +0530

    Add logs

--- installer/src/essentials.rs
+++ installer/src/essentials.rs
@@ -18,7 +18,10 @@ impl<'a> Essentials<'a> {
 
     /// chroot into the system
     /// It is imperative that this should be called first before executing any other fns.
+    /// Instead of calling arch-chroot, chroot is being called directly.
+    /// Followed instructions from [here](https://wiki.archlinux.org/title/Chroot#Using_chroot)
     pub fn chroot(&mut self) -> Result<()>{
+        self.shell.log("Entering chroot.");
         self.shell.run_with_args("mount", "-t proc /proc /mnt/proc/")?;
         self.shell.run_with_args("mount", "-t sysfs /sys /mnt/sys/")?;
         self.shell.run_with_args("mount", "-o bind /dev /mnt/dev/ ")?;
@@ -28,14 +31,25 @@ impl<'a> Essentials<'a> {
         std::os::unix::fs::chroot("/sandbox")?;
         std::env::set_current_dir("/")?;
 
+        self.shell.log("Entered chroot.");
         
+        self.shell.log("Sourcing bash profiles from chroot.");
         self.shell.run_with_args("source", "/etc/profile")?;
         self.shell.run_with_args("source", "~/.bashrc")?;
         self.is_chroot = true;
+
+
+        self.shell.log("Completed entering chroot.");
         Ok(())
     }
 
+    /// Sets the swap size.
+    /// Size is the number of 1M blocks on disk.
+    /// eg. 1GB swap = 1024, 8GB = 8096...
+    /// Should be run in a multithreaded manner. There is no point in waiting for this to complete.
+    /// But, must panic if the operation fails as that would affect the whole system.
     pub fn initialize_swap(&mut self, size: usize) -> Result<()> {
+        self.shell.log("Initializing Swap.");
         if !self.is_chroot{
             self.shell.log("Cannot initialize swap, not in chroot.");
             return Err(anyhow!("Cannot initialize swap, not in chroot."));
@@ -60,7 +74,7 @@ impl<'a> Essentials<'a> {
         
 
 
-       self.shell.log(&"Appending swap to fstab.");
+       self.shell.log("Appending swap to fstab.");
         let mut fstab = match OpenOptions::new()
             .append(true)
             .create(true)
@@ -83,13 +97,23 @@ impl<'a> Essentials<'a> {
         Ok(())
     }
 
+    pub fn set_timezones(&mut self) -> Result<()>{
+
+    }
+
    
+    /// Exits chroot
+    /// Also unmounts all the partitions that were mounted during the chroot setup.
+    /// Follows the guide [here](https://wiki.archlinux.org/title/Chroot#Using_chroot)
     pub fn exit_chroot(&mut self) -> Result<()>{
+        self.shell.log("Started exit chroot.");
+
         self.shell.run_and_wait("exit")?;
         std::env::set_current_dir("/")?;
         self.shell.run_and_wait_with_args("umount", "--recursive /path/to/new/root")?;
 
         self.is_chroot = false;
+        self.shell.log("Exit chroot successful.");
         Ok(())
     }
 }
